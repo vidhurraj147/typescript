e43e1e2bfe82b67f29b81904d2b8f47fcf16fac2
vidhurraj147 <vidhurraj147@gmail.com>
2018-12-18 00:03:35 -0500

    new files OR edits added

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import {PersonExport} from './PersonExport';
+import * as _ from 'lodash';
 
 var pe = new  PersonExport();
 pe.firstName = "Rahul";
 pe.lastName = "Kadgekar";
 console.log(pe.firstName +", "+pe.lastName);
 console.log(pe);
+
+var myArray = [4,5,8,6,3,9,8,2];
+console.log(_.reverse(myArray));
--- out/index.js
+++ out/index.js
@@ -1,8 +1,18 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var PersonExport_1 = require("./PersonExport");
+var _ = __importStar(require("lodash"));
 var pe = new PersonExport_1.PersonExport();
 pe.firstName = "Rahul";
 pe.lastName = "Kadgekar";
 console.log(pe.firstName + ", " + pe.lastName);
 console.log(pe);
+var myArray = [4, 5, 8, 6, 3, 9, 8, 2];
+console.log(_.reverse(myArray));
