a002c6e852e32498e40b00ce73302a7f72d8df38
vidhurraj147 <vidhurraj147@gmail.com>
2018-12-17 21:05:05 -0500

    Added Person class does the basic tasks

--- classes.js
+++ classes.js
@@ -1,11 +1,25 @@
 var Person = /** @class */ (function () {
-    function Person() {
+    // constructor(){
+    //     this.firstName = "Added via constructor";
+    //     this.lastName = "Added via constructor";
+    // }
+    function Person(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
+    Person.prototype.getFullName = function () {
+        return this.firstName + " " + this.lastName;
+    };
     return Person;
 }());
-var aPerson = new Person();
+var aPerson = new Person("Parameterized_FirstName", "Parameterized_LastName");
+console.log(aPerson.getFullName());
 console.log(aPerson.firstName);
-aPerson.firstName = "Rahul Kadgekar";
+aPerson.firstName = "FIRSTNAME";
+aPerson.lastName = "LASTNAME";
 console.log(aPerson.firstName);
-var bPerson;
-bPerson.lastName = "LAST NAME ADDED";
+console.log(aPerson.getFullName());
+// var bPerson : Person;
+// bPerson = new Person();
+// bPerson.firstName = "B FIRST NAME ADDED";
+// bPerson.lastName = "B LAST NAME ADDED";
--- classes.ts
+++ classes.ts
@@ -2,12 +2,28 @@ class Person {
     firstName: string;
     lastName: string;
     age: number;
+    // constructor(){
+    //     this.firstName = "Added via constructor";
+    //     this.lastName = "Added via constructor";
+    // }
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName(){
+        return this.firstName + " " + this.lastName;
+    }
 }
 
-var aPerson = new Person();
+var aPerson = new Person("Parameterized_FirstName","Parameterized_LastName");
+console.log(aPerson.getFullName());
 console.log(aPerson.firstName);
-aPerson.firstName = "Rahul Kadgekar";
+aPerson.firstName = "FirstName";
+aPerson.lastName = "LastName";
 console.log(aPerson.firstName);
+console.log(aPerson.getFullName());
 
-var bPerson : Person;
-bPerson.lastName = "LAST NAME ADDED";
+// var bPerson : Person;
+// bPerson = new Person();
+// bPerson.firstName = "B FIRST NAME ADDED";
+// bPerson.lastName = "B LAST NAME ADDED";
